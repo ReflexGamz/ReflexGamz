a6edf56184652d25da3755b0eaac00a03bd7ffc1
Andreas Jacob <andreasjacob@Andreass-Air.lan>
2024-05-26 17:52:04 -0700

    Intial Commit

---
+++ ReflexGamz/ReflexGamzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflexGamzApp.swift
+//  ReflexGamz
+//
+//  Created by Andreas Jacob on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ReflexGamzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
